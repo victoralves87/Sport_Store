12239a7fb2a20d697f91725348cfcca0b9e34744
victor alves <voctorralves@gmail.com>
2023-03-11 00:03:05 -0300

    ajeitando js

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 /*
-function grande(){
-    let div = document.getElementById("aumenta")
-    div.style.width = '300px';
+function exibir(categoria){
+   // alert(categoria);
+
+   let elementos = document.getElementById("produtos")
+console.log(elementos)
+
+for(var i=0;i<elementos.length;i++){
+    console.log(elementos[i].id)
+    if(categoria == elementos[i].id){
+        elementos[i].style = "display:block"
+    }else{
+        lementos[i].style = "display:none"
+    }
 }
 
+}
 */
 
-function exibir(categoria){
-    alert(categoria)
-
+function grande(){
+    let div =document.getElementById('aumenta')
+    div.style.width = "300px"
 }
 
 
-let elementos = document.getElementById("produtos")
-console.log(elementos);
-for(var i=0;i < elementos.length; i++){
-    console.log(elementos[i].id)
+function grande(){
+    let div =document.getElementById('chama')
+    div.style.width = "300px"
 }
+
+
+function grande(){
+    let div =document.getElementById('arroz')
+    div.style.width = "300px"
+}
+
+
